db6da6e9abc1be32f3bac3b55ba7a9b29901e58d
Sergey Krikun <sergey.a.krikun@gmail.com>
2023-02-06 22:01:29 +0300

    feature: add reading .env

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import 'dotenv/config';
+
+const port = process.env.PORT || 4000;
+
+const messageOnServerStart = `\x1b[33mThe Server was started at http://localhost:${port} \x1b[0m`;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +14,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(4000);
+  await app.listen(port, () => console.log(messageOnServerStart));
 }
 bootstrap();
