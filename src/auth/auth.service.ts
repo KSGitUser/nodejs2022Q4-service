534ae5822b42fbb1d38d40fd7972b590645315cb
Sergey Krikun <sergey.a.krikun@gmail.com>
2023-02-26 00:28:14 +0300

    feat: authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(
     login: string,
@@ -16,5 +20,12 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: User) {
+    const payload = { login: user.login, userId: user.id, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
