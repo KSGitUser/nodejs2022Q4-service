27c83fe54a7daa7fc3476504104270d63cf51882
Sergey Krikun <sergey.a.krikun@gmail.com>
2023-02-28 01:28:38 +0300

    feat: LoggingService should log all errors

--- src/logger/custom-logger.service.ts
+++ src/logger/custom-logger.service.ts
@@ -2,8 +2,8 @@ import { ConsoleLogger } from '@nestjs/common';
 
 export class CustomLogger extends ConsoleLogger {
   error(message: any, stack?: string, context?: string, ...args) {
-    // add your tailored logic here
     super.error(message, stack, context, ...args);
+    console.log(message);
   }
 
   logReq(request: any, response: any) {
--- src/request-logging/request-logging.middleware.ts
+++ src/request-logging/request-logging.middleware.ts
@@ -1,23 +1,60 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 import { CustomLogger } from '../logger/custom-logger.service';
 
 @Injectable()
 export class RequestLoggingMiddleware implements NestMiddleware {
   constructor(private readonly loggingService: CustomLogger) {}
-  use(req: Request, res: Response, next: () => void) {
-    const { method, baseUrl, query, body } = req;
+  async use(req: Request, res: Response, next: () => void) {
+    try {
+      const { method, baseUrl, query, body } = req;
 
-    res.on('finish', () => {
-      const response = {
-        statusCode: res.statusCode,
-        message: res.statusMessage,
-      };
-      this.loggingService.logReq(
-        { method, url: baseUrl, query, body },
-        response,
-      );
-    });
-    next();
+      res.on('finish', () => {
+        const response = {
+          statusCode: res.statusCode,
+          message: res.statusMessage,
+        };
+        this.loggingService.logReq(
+          { method, url: baseUrl, query, body },
+          response,
+        );
+      });
+      res.on('error', (error) => {
+        throw error;
+      });
+      await next();
+    } catch (error) {
+      let customError;
+      if (error instanceof HttpException) {
+        const status = error.getStatus();
+        res.status(status).json({
+          statusCode: status,
+          message: error.message,
+        });
+        customError = {
+          statusCode: status,
+          message: error.message,
+          error: error,
+        };
+      } else {
+        const status = HttpStatus.INTERNAL_SERVER_ERROR;
+        res.status(status).json({
+          statusCode: status,
+          message: 'Internal server error',
+        });
+        customError = {
+          statusCode: status,
+          message: error.message,
+          error: error,
+        };
+      }
+
+      this.loggingService.error(customError);
+    }
   }
 }
